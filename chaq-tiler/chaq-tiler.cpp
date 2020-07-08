f140c968d5a61b738de1c5930743a7966834e243
chaquator <chaquator@yahoo.com>
2020-07-08 16:04:35 -0500

    beginning to lay groundwork for windows

--- chaq-tiler/chaq-tiler.cpp
+++ chaq-tiler/chaq-tiler.cpp
@@ -2,7 +2,7 @@
 #include <Shobjidl.h>
 
 #include <cstddef>
-#include <cmath>
+#include <cstdint>
 #include <type_traits>
 
 #include <string>
@@ -14,7 +14,10 @@
 
 #include <cassert>
 
+#include <algorithm>
 #include <iostream>
+
+// TODO: pull code out into different places
 
 // Usings and typedefs
 using namespace std::literals;
@@ -22,7 +25,7 @@ using namespace std::literals;
 // Structs and classes
 
 struct Rule {
-	enum SingleAction {
+	enum class SingleAction {
 		None,
 		Unmaximize,
 		Maximize
@@ -60,29 +63,30 @@ struct Rule {
 	SingleAction Action;
 };
 
-struct Point {
-	int x;
-	int y;
+struct Vec {
+	using vec_t = std::int32_t;
+	vec_t x;
+	vec_t y;
 };
-inline Point operator +(Point a, Point b) {
+inline Vec operator +(Vec a, Vec b) {
 	return { a.x + b.x, a.y + b.y };
 }
-inline Point operator -(Point a, Point b) {
+inline Vec operator -(Vec a, Vec b) {
 	return { a.x - b.x, a.y - b.y };
 }
 template <typename Scalar>
-inline Point mul(Scalar scalar, Point p) {
+inline static Vec mul(Scalar scalar, Vec p) {
 	return {
-		static_cast<int>(scalar * static_cast<Scalar>(p.x)),
-		static_cast<int>(scalar * static_cast<Scalar>(p.y))
+		static_cast<Vec::vec_t>(scalar * static_cast<Scalar>(p.x)),
+		static_cast<Vec::vec_t>(scalar * static_cast<Scalar>(p.y))
 	};
 }
 template <typename Scalar>
-inline Point operator *(Scalar a, Point b) {
+inline Vec operator *(Scalar a, Vec b) {
 	return mul(a, b);
 }
 template <typename Scalar>
-inline Point operator *(Point b, Scalar a) {
+inline Vec operator *(Vec b, Scalar a) {
 	return mul(a, b);
 }
 
@@ -90,35 +94,46 @@ inline Point operator *(Point b, Scalar a) {
 // whenever calculating, determine size based on whether taskbar is also present (will also need to account for orientation and position of it too)
 struct Desktop {
 	int margin; // Space between each window (and from the monitor edge to the window)
-	Point monitor_upper_left;
-	Point monitor_lower_right;
+	Vec monitor_upper_left;
+	Vec monitor_dimensions;
 };
 
 struct Window {
 	HWND handle;
 	bool floating;
-	// TODO: complete struct, things like rules applied
+	std::bitset<10> current_tags;
+	Rule::SingleAction action;
 
-	Window(HWND hdnl, bool flt) : handle(hdnl), floating(flt) {}
+	std::wstring title;
+	std::wstring class_name;
+
+	Window(HWND handle, std::wstring_view& title, std::wstring_view& class_name):
+		handle(handle), floating(false), current_tags(0), action(Rule::SingleAction::None),
+		title(title), class_name(class_name) {
+	}
 };
 
 // Globals
 
 namespace Globals {
 	std::vector<Window> Windows;
+	HMONITOR PrimaryMonitor;
+	Desktop PrimaryDesktop;
 }
 
 // Function definitions
 
 // Debug utilities
-static void debug(std::string&& text);
+void PrintDebugString(std::string_view&& text);
 
 // Views
 template <typename Container>
 void cascade(const Container& windows, const Desktop& desktop);
 
-static BOOL CALLBACK EnumWindowsProc(HWND, LPARAM);
-static bool shouldManageWindow(HWND);
+static Window ApplyRules(HWND window, LONG style, LONG exStyle, std::wstring_view& title, std::wstring_view& class_name);
+static bool ShouldManageWindow(HWND);
+static BOOL CALLBACK ScanWindows(HWND, LPARAM);
+static HMONITOR GetPrimaryMonitorHandle();
 
 int WinMain(HINSTANCE, HINSTANCE, LPSTR, int);
 
@@ -127,50 +142,63 @@ constexpr static std::size_t buflen = 128;
 // Including config
 #include "config.h"
 
-void debug(std::string&& text) {
+void PrintDebugString(std::string_view&& text) {
 	std::cout << "DEBUG: " << text << std::endl;
 }
+#ifdef NDEBUG
+#define debug(s) ((void)0);
+#else
+#define debug(s) PrintDebugString(s);
+#endif
 
 template <typename Container>
 void cascade(const Container& windows, const Desktop& desktop) {
 	// TODO: floating windows
 
 	using SizeType = typename Container::size_type;
+	using DiffType = typename Container::difference_type;
 	using Iterator = typename Container::const_iterator;
 
 	if (windows.empty()) return; // Short circuit break
 
 	// Customize cascade here
 	constexpr float window_factor = 0.35f; // Window will be 35% dimension of screen
-	Point cascade_delta = {
-		static_cast<int>(0.6f * static_cast<float>(desktop.margin)), // Slightly less x increase with every y increase
+	Vec cascade_delta = {
+		static_cast<Vec::vec_t>(0.2f * static_cast<float>(desktop.margin)), // Less x increase with every y increase
 		desktop.margin
 	};
 
-	Point desktop_dimensions = desktop.monitor_lower_right - desktop.monitor_upper_left;
-	Point window_dimensions = window_factor * desktop_dimensions;
+	Vec window_dimensions = window_factor * desktop.monitor_dimensions;
 
 	// Calculate, accounting for margin, how many cascades are needed for the screen
-	int working_desktop_height = desktop_dimensions.y - 2 * cascade_delta.y - window_dimensions.y;
-	int cascade_total_height = (static_cast<int>(windows.size() - 1) * cascade_delta.y); // cascade height only considering the margin
+	Vec::vec_t working_desktop_height = desktop.monitor_dimensions.y - 2 * cascade_delta.y - window_dimensions.y;
+	Vec::vec_t cascade_total_height = (static_cast<Vec::vec_t>(windows.size() - 1) * cascade_delta.y); // cascade height only considering the margin
 	std::size_t cascades = static_cast<std::size_t>(cascade_total_height / working_desktop_height); // amount of cascades
 	std::size_t cascade_leftover = static_cast<std::size_t>(cascade_total_height % working_desktop_height); // remaining height
 
 	// # of windows to draw = ceil(desktop_height / window_height)
 	SizeType windows_per_full_cascade = static_cast<SizeType>((working_desktop_height / cascade_delta.y)) + ((working_desktop_height % cascade_delta.y != 0) ? 1 : 0);
 
-	auto single_cascade = [&](SizeType amount, Iterator start, std::size_t current_cascade) {
-		for (std::size_t index = 0; index < static_cast<int>(amount); ++index, ++start) {
+	auto single_cascade = [&] (SizeType amount, Iterator start, std::size_t current_cascade) {
+		for (SizeType index = 0; index < amount; ++index, ++start) {
 			/*
-			Point ul = { desktop.margin, desktop.margin };
-			Point cascade_offset = { current_cascade * (window_dimensions.x + cascade_delta.x), 0 };
-			Point base = ul + cascade_offset;
-			Point travel = index * cascade_delta;
+			Point upper_left = { desktop.margin, desktop.margin };
+			Point cascade_offset = { static_cast<Vec::vec_t>(current_cascade) * (window_dimensions.x + cascade_delta.x), 0 };
+			Point base = upper_left + cascade_offset;
+			Point travel = static_cast<Vec::vec_t>(index) * cascade_delta;
 			Point pos = base + travel;
 			*/
+			Vec pos = {
+				static_cast<Vec::vec_t>(desktop.margin) + static_cast<Vec::vec_t>(current_cascade) * (window_dimensions.x + cascade_delta.x) + static_cast<Vec::vec_t>(index) * cascade_delta.x,
+				static_cast<Vec::vec_t>(desktop.margin) + static_cast<Vec::vec_t>(index) * cascade_delta.y
+			};
 
-			Point pos = (Point{ desktop.margin, desktop.margin } + Point{ static_cast<int>(current_cascade) * (window_dimensions.x + cascade_delta.x), 0 }) + (index * cascade_delta);
-			SetWindowPos(start->handle, HWND_BOTTOM, pos.x, pos.y, window_dimensions.x, window_dimensions.y, SWP_NOACTIVATE);
+			::SetWindowPos(start->handle,
+				HWND_BOTTOM,
+				static_cast<int>(pos.x), static_cast<int>(pos.y),
+				static_cast<int>(window_dimensions.x), static_cast<int>(window_dimensions.y),
+				SWP_NOACTIVATE
+			);
 		}
 	};
 
@@ -186,52 +214,16 @@ void cascade(const Container& windows, const Desktop& desktop) {
 
 	// Leftovers
 	if (std::distance(current_window, windows.cend()) > 0) {
-		assert(std::distance(current_window, windows.cend()) < windows_per_full_cascade);
+		assert(std::distance(current_window, windows.cend()) < static_cast<DiffType>(windows_per_full_cascade));
 		single_cascade(std::distance(current_window, windows.cend()), current_window, current_cascade);
 	}
 }
 
-// Definitions
-bool shouldManageWindow(HWND window) {
-	// Empty window name filtering
-	if (GetWindowTextLength(window) == 0) return false;
+Window ApplyRules(HWND window, LONG style, LONG exStyle, std::wstring_view& title, std::wstring_view& class_name) {
+	Window new_window { window, title, class_name };
+	for (auto& rule : Config::WindowRuleList) {
+		if (!rule.Manage) continue; // Skip rules that exclude windows
 
-	// Visibility Filtering
-	if (!IsWindowVisible(window)) return false;
-
-	// Attributes
-	LONG style = GetWindowLong(window, GWL_STYLE);
-	if (style == 0) {
-		debug("Style 0");
-		// TODO: Error
-		return false;
-	}
-	LONG exStyle = GetWindowLong(window, GWL_EXSTYLE);
-	if (exStyle == 0) {
-		debug("ExStyle 0");
-		// TODO: Error
-		return false;
-	}
-
-	TCHAR title_buf[buflen];
-	int len = GetWindowText(window, title_buf, buflen);
-	if (len == 0) {
-		debug("Title len 0");
-		// TODO: Error
-		return false;
-	}
-	std::wstring_view title{ title_buf, static_cast<std::size_t>(len) };
-
-	TCHAR class_buf[buflen];
-	len = GetClassName(window, class_buf, buflen);
-	if (len == 0) {
-		debug("Class len 0");
-		// TODO: Error
-		return false;
-	}
-	std::wstring_view class_name{ class_buf, static_cast<std::size_t>(len) };
-
-	for (auto& rule : window_rule_list) {
 		// Class name filtering
 		if (!rule.ClassName.empty() && class_name.find(rule.ClassName) == std::string_view::npos) continue;
 
@@ -244,60 +236,122 @@ bool shouldManageWindow(HWND window) {
 		// Extended Style filtering
 		if ((exStyle & rule.ExStyle) != rule.ExStyle) continue;
 
-		if (!rule.Manage) return false;
+		new_window.floating = rule.Floating;
+		new_window.current_tags |= rule.TagMask;
+		new_window.action = rule.Action;
 	}
-
-	return true;
+	return new_window;
 }
 
-BOOL CALLBACK EnumWindowsProc(HWND window, LPARAM) {
+// Definitions
+bool ShouldManageWindow(HWND window, LONG style, LONG exStyle, std::wstring_view& title, std::wstring_view& class_name) {
+	// TODO: virtual desktop filtering
+
+	// Empty window name filtering
+	if (GetWindowTextLength(window) == 0) return false;
+
+	// Visibility Filtering
+	if (!IsWindowVisible(window)) return false;
+
+	// Primary monitor only filtering
+	if (Config::PrimaryMonitorWindowsOnly) {
+		HMONITOR monitor = MonitorFromWindow(window, MONITOR_DEFAULTTONULL);
+		if (monitor != Globals::PrimaryMonitor) return false;
+	}
+
+	bool should_manage = std::any_of(Config::WindowRuleList.cbegin(), Config::WindowRuleList.cend(), [&] (auto& rule) -> bool {
+		// Class name filtering
+		if (!rule.ClassName.empty() && class_name.find(rule.ClassName) == std::string_view::npos) return false;
+
+		// Title name filtering
+		if (!rule.TitleName.empty() && title.find(rule.TitleName) == std::string_view::npos) return false;
+
+		// Style filtering
+		if ((style & rule.Style) != rule.Style)	return false;
+
+		// Extended Style filtering
+		if ((exStyle & rule.ExStyle) != rule.ExStyle) return false;
+
+		// Manage window (return true) if rule says window should be managed
+		return rule.Manage;
+	});
+
+	return should_manage;
+}
+
+BOOL CALLBACK ScanWindows(HWND window, LPARAM) {
 	// TODO: make WS_MAXIMIZE windows unmaximize, try ShowWindow(window, SW_RESTORE)
+	// Attributes
+	LONG style = GetWindowLong(window, GWL_STYLE);
+	if (style == 0) {
+		debug("Style 0");
+		// TODO: Error
+		return false;
+	}
 
-	if (shouldManageWindow(window)) {
-		TCHAR buf[buflen];
-		int len = GetWindowText(window, buf, buflen);
-		if (len == 0) {
-			debug("Title len 0");
-			// Error
-			return FALSE;
-		}
-		std::wstring_view title{ buf, static_cast<std::size_t>(len) };
+	LONG exStyle = GetWindowLong(window, GWL_EXSTYLE);
+	if (exStyle == 0) {
+		debug("ExStyle 0");
+		// TODO: Error
+		return false;
+	}
 
-		TCHAR class_buf[buflen];
-		len = GetClassName(window, class_buf, buflen);
-		if (len == 0) {
-			debug("Class len 0");
-			// TODO: Error
-			return false;
-		}
-		std::wstring_view class_name{ class_buf, static_cast<std::size_t>(len) };
+	TCHAR title_buf [buflen];
+	int len = GetWindowText(window, title_buf, buflen);
+	if (len == 0) {
+		debug("Title len 0");
+		// TODO: Error
+		return false;
+	}
+	std::wstring_view title { title_buf, static_cast<std::size_t>(len) };
 
-		// TODO: decide what to do with windows on different monitors
-		// HMONITOR monitor = MonitorFromWindow(window, MONITOR_DEFAULTTONULL);
+	TCHAR class_buf [buflen];
+	len = GetClassName(window, class_buf, buflen);
+	if (len == 0) {
+		debug("Class len 0");
+		// TODO: Error
+		return false;
+	}
+	std::wstring_view class_name { class_buf, static_cast<std::size_t>(len) };
 
+	if (ShouldManageWindow(window, style, exStyle, title, class_name)) {
 		std::wcout << title << std::endl;
 
-		Globals::Windows.emplace_back(window, false);
+		Window new_window = ApplyRules(window, style, exStyle, title, class_name);
+		Globals::Windows.push_back(std::move(new_window));
 	}
 
 	return TRUE;
 }
 
+HMONITOR GetPrimaryMonitorHandle() {
+	return MonitorFromPoint(POINT { 0, 0 }, MONITOR_DEFAULTTOPRIMARY);
+}
+
 int WinMain(HINSTANCE Instance, HINSTANCE, LPSTR, int CmdShow) {
 	FILE* fDummy;
-
 	AllocConsole();
 	freopen_s(&fDummy, "CONOUT$", "w", stdout);
 
-	EnumWindows(EnumWindowsProc, NULL);
-
-	Desktop desk = {
-		.margin = 50,
-		.monitor_upper_left = {0, 0},
-		.monitor_lower_right = {2560, 1440}
+	// Setup desktop
+	Globals::PrimaryMonitor = GetPrimaryMonitorHandle();
+	MONITORINFO monitor_info = {
+		.cbSize = sizeof(MONITORINFO)
+	};
+	if (!GetMonitorInfo(Globals::PrimaryMonitor, &monitor_info)) {
+		// TODO: Error
+		debug("Failed to get primary monitor")
+			return EXIT_FAILURE;
+	}
+	Globals::PrimaryDesktop = Desktop {
+		.margin = Config::DefaultMargin,
+		.monitor_upper_left = Vec { monitor_info.rcWork.left, monitor_info.rcWork.top },
+		.monitor_dimensions = Vec { monitor_info.rcWork.right - monitor_info.rcWork.left, monitor_info.rcWork.bottom - monitor_info.rcWork.top }
 	};
 
-	cascade(Globals::Windows, desk);
+	EnumWindows(ScanWindows, NULL);
+
+	cascade(Globals::Windows, Globals::PrimaryDesktop);
 
 	while (true) {
 		if (GetAsyncKeyState(VK_NUMPAD0)) break;
--- chaq-tiler/config.h
+++ chaq-tiler/config.h
@@ -1,11 +1,20 @@
 // Window rule list
+// Rules will be applied in order listed, any conflicting rules will be decided by whichever rule comes last, so list them from most general -> most specific
 // Class substring, Window substring, Window style, Extended window style, Should Manage, Floating, Tag Mask, Action
-constexpr std::array<Rule, 6> window_rule_list = {
-	Rule{L"", L"", WS_DISABLED, WS_EX_TOOLWINDOW, false, false, 0, Rule::SingleAction::None}, // Default filtering
-	Rule{L"", L"Settings", 0, 0, false, false, 0, Rule::SingleAction::None}, // Hidden settings window
-	Rule{L"", L"Microsoft Store", 0, 0, false, false, 0, Rule::SingleAction::None}, // Hidden Microsoft Store window
-	Rule{L"Progman", L"Program Manager", 0, 0, false, false, 0, Rule::SingleAction::None}, // Progman
+namespace Config {
+	constexpr const std::array<Rule, 7> WindowRuleList = {
+		Rule{L"", L"", NULL, NULL, true, false, 0, Rule::SingleAction::Unmaximize}, // Default rule
 
-	Rule{L"CEF-OSC-WIDGET", L"NVIDIA GeForce Overlay", 0, 0, false, false, 0, Rule::SingleAction::None}, // NVIDIA Geforce
-	Rule{L"Windows.UI.Core.CoreWindow", L"", 0, 0, false, false, 0, Rule::SingleAction::None} // Windows UI
-};
+		Rule{L"", L"", WS_DISABLED, WS_EX_TOOLWINDOW, false, false, 0, Rule::SingleAction::None}, // Disabled/Toolwindow filtering
+		Rule{L"", L"Settings", 0, 0, false, false, 0, Rule::SingleAction::None}, // Hidden settings window
+		Rule{L"", L"Microsoft Store", 0, 0, false, false, 0, Rule::SingleAction::None}, // Hidden Microsoft Store window
+		Rule{L"Progman", L"Program Manager", 0, 0, false, false, 0, Rule::SingleAction::None}, // Progman
+
+		Rule{L"CEF-OSC-WIDGET", L"NVIDIA GeForce Overlay", 0, 0, false, false, 0, Rule::SingleAction::None}, // NVIDIA Geforce
+		Rule{L"Windows.UI.Core.CoreWindow", L"", 0, 0, false, false, 0, Rule::SingleAction::None} // Windows UI
+	};
+
+	constexpr const int DefaultMargin = 50;
+
+	constexpr const bool PrimaryMonitorWindowsOnly = true;
+}
